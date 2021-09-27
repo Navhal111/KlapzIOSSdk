41775f0affcb9a7126510ebdbccada64dd5a362b
Navhal Ritesh <ylight528@gmail.com>
2021-09-27 12:43:42 +0530

    Initial Commit

---
+++ Shared/KlapzSdkIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlapzSdkIOSApp.swift
+//  Shared
+//
+//  Created by Kakashi on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct KlapzSdkIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
